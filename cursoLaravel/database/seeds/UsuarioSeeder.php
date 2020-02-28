4be200b871fa39e3257111a0cedc7987f7b5ea3d
Vitor Rubim <vitorrubim1@hotmail.com>
2020-02-27 21:06:28 -0300

    Criado um adm com make:seed

--- cursoLaravel/database/seeds/UsuarioSeeder.php
+++ cursoLaravel/database/seeds/UsuarioSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class UsuarioSeeder extends Seeder
 {
@@ -11,6 +12,28 @@ class UsuarioSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //PARA CRIAR REGISTROS
+
+        $dados = [
+            'name'=>"Vitor",
+            'email'=>"admin@mail.com",
+            'password'=>bcrypt("1234"),
+        ];
+
+        //PARA VER SE JA TEM UM USER IGUAL
+        if(User::where('email','=',$dados['email'])->count()){
+
+            $usuario = User::where('email','=',$dados['email'])->first();
+            $usuario->update($dados);
+
+            echo "Usuário alterado"; 
+        }else{
+
+            //CASO NAO TENHA CRIA O USER
+            User::create($dados);
+            
+            echo "Usuário criado"; 
+
+        }
     }
 }
